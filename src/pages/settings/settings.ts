acb30cab29d32bb3d418e092d2af4d71bd7eaf5c
antoinedelise976@hotmail.fr <maracapraro@libero.it>
2018-02-09 15:45:53 +0100

    merge
    enzugo

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-settings',
@@ -7,11 +8,34 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SettingsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingsPage');
   }
 
+presentAlert() {
+  let alert = this.alertCtrl.create({
+    title: 'Panne déclenchée',
+    subTitle: 'La panne de la machine à bien été simulée.',
+    buttons: ['continuer']
+  });
+  alert.present();
+}
+  
+	trigger_alert() {
+		this.presentAlert();
+		/*if(this.flag)
+			return;
+		this.flag= true;
+		this.auth.login(this.registerCredentials).subscribe(data => {
+        console.log(data);
+        if(true){
+			this.nav.setRoot('MenuPage');
+		}
+    }, error => {
+            console.log(error);
+	});*/
+  }
 }
