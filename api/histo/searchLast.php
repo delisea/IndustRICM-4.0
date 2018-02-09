2226a91b626ffca962cfff7ff70c8451f4528e1c
delisea <antoinedelise976@hotmail.fr>
2018-02-09 23:15:36 +0100

    Update searchLast.php

--- api/histo/searchLast.php
+++ api/histo/searchLast.php
@@ -7,13 +7,21 @@ include_once '../config/database.php';
 $database = new Database();
 $db = $database->getConnection();
 
-$query = 'SELECT `HistoS`.*, `Staff`.name FROM `HistoS`, (SELECT `idStaff`, MAX(`date`) as dateM FROM `HistoS` GROUP BY idStaff) AS LASTH, `Staff` WHERE `HistoS`.idStaff = LASTH.idStaff AND `HistoS`.date = LASTH.dateM AND `Staff`.working=1 AND `Staff`.id=`HistoS`.idStaff';
+$query = 'SELECT * FROM Staff LEFT JOIN (SELECT `HistoS`.* FROM `HistoS`, (SELECT `idStaff`, MAX(`date`) AS MD FROM `HistoS` GROUP BY `idStaff`) AS GB WHERE `HistoS`.date=GB.MD AND `HistoS`.idStaff=GB.idStaff) AS FI ON Staff.id = FI.idStaff WHERE working = 1 AND idStaff IS NOT NULL';
 $stmt = $db->prepare($query);
 $stmt->execute();
 $result = array();
 $result['staff'] = array();
-while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {	 
 	extract($row);
-
-	
+	$staff_item = array(
+		'id' => $id,
+		'name' => $name,
+		'working' => $working,
+		'date' => $date,
+		'locationX' => $locationX,
+		'locationY' => $locationY
+	);
+	array_push($staffs_arr['records'], $staff_item);
 }
+echo json_encode($staffs_arr)
