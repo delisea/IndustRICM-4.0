6fd345e5ba4901cf637af18fe4cad876d16e03d0
antoinedelise976@hotmail.fr <maracapraro@libero.it>
2018-02-09 15:11:31 +0100

    alert

--- PanneApp/src/pages/home/home.ts
+++ PanneApp/src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,35 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+constructor(private alertCtrl: AlertController) {
+
+}
+
+presentAlert() {
+  let alert = this.alertCtrl.create({
+    title: 'Panne déclenchée',
+    subTitle: 'La panne de la machine à bien été simulée.',
+    buttons: ['continuer']
+  });
+  alert.present();
+}
+
   constructor(public navCtrl: NavController) {
 
   }
-
+  
+	trigger_alert() {
+		this.presentAlert();
+		/*if(this.flag)
+			return;
+		this.flag= true;
+		this.auth.login(this.registerCredentials).subscribe(data => {
+        console.log(data);
+        if(true){
+			this.nav.setRoot('MenuPage');
+		}
+    }, error => {
+            console.log(error);
+	});*/
+  }
 }
