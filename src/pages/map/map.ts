85cc428189e8584a3403081c1158707ac330a63c
Enzo Molion <enzo.molion.0@gmail.com>
2018-02-10 08:09:03 +0100

    whaou

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -9,6 +9,7 @@ export interface mapItem{
   working: string;
   locationX: string;
   locationY: string;
+  floor: string;
   date: string;
 }
 export interface dataItem{
@@ -21,12 +22,39 @@ export interface dataItem{
   templateUrl: 'map.html'
 })
 export class MapPage {
-  @ViewChild('map') mapElement: ElementRef;
-  map: any;
+  @ViewChild('map1') mapElement: ElementRef;
+  map1: any;
+  map2: any;
+  map3: any;
   loaded: any = 0;
 
   selectedLocalisable: string = "all";
   selectedLevel: string = "level1";
+
+  basement: string = '../../../assets/basement.PNG';
+  firstLevel: string = '../../../assets/firstlevel.PNG';
+  secondLevel: string = '../../../assets/secondlevel.PNG';
+
+  mapW = 2000 ;
+  mapH = 2000;
+
+  IconGreen = Leaflet.icon({
+    iconUrl: "../../assets/imgs/pointer_green.png",
+    iconSize: [30, 30], // size of the icon
+    iconAnchor: [15, 30]
+  });
+  IconRed = Leaflet.icon({
+    iconUrl: "../../assets/imgs/pointer_red.png",
+    iconSize: [30, 30], // size of the icon
+    iconAnchor: [15, 30]
+  });
+  IconBlue = Leaflet.icon({
+    iconUrl: "../../assets/imgs/pointer_blue.png",
+    iconSize: [30, 30], // size of the icon
+    iconAnchor: [15, 30]
+  });
+
+  apiURL = "http://closed.power-heberg.com/industRICM/api/";
 
   constructor(
     public navCtrl: NavController,
@@ -36,50 +64,99 @@ export class MapPage {
 	}
 
 	ionViewDidLoad() {
-		if(!this.loaded) {
-			this.loadmap();
-			this.loaded = 1;
-		}
+    this.loadmap("map1", this.map1, 0);
+    this.loadmap("map2", this.map2, 1);
+    this.loadmap("map3", this.map3, 2);
+
 	}
 
   ionViewCanLeave(){
-    document.getElementById("map").outerHTML = "";
+    // FIXME
+    document.getElementById("map1").outerHTML = "";
+    document.getElementById("map2").outerHTML = "";
+    document.getElementById("map3").outerHTML = "";
   }
 
-	loadmap() {
-    this.map = Leaflet.map("map", {attributionControl: false}).fitWorld();
-    Leaflet.control.scale({imperial: false}).addTo(this.map);
-    Leaflet.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      minZoom: 0,
-      maxZoom: 18,
-    }).addTo(this.map);
+  onMapClick(e){
+        console.log(e.latlng.lng, e.latlng.lat);
+  }
 
-    var IconGreen = Leaflet.icon({
-      iconUrl: "../../assets/imgs/pointer_green.png",
-      iconSize: [30, 30], // size of the icon
-      iconAnchor: [15, 30]
-    });
+	loadmap(str: string, myMap: any, floor: number) {
 
-  	let apiURL = "http://closed.power-heberg.com/industRICM/api/";
-  	//let data = {staff: [{username: "Bernard", idStaff: "1", date: "42", longitude: "5", latitude: "7"},{username: "Maxime",idStaff: "2", date: "43", longitude: "8", latitude: "7"}]}
-	
-  	this.httpClient.post<dataItem>(apiURL+'histo/searchLast.php', {}/*JSON.stringify(credentials)*/).subscribe(
+    myMap = Leaflet.map(str, {attributionControl: false, noWrap: true}).fitWorld();
+    // Leaflet.control.scale({imperial: false}).addTo(myMap);
+    let mapUrl: string ;
+    switch (str) {
+      case "map1":
+        mapUrl = this.basement;
+        break;
+      case "map2":
+        mapUrl = this.firstLevel;
+        break;
+      case "map3":
+        mapUrl = this.secondLevel;
+        break;
+      
+      default:
+        throw new Error("Unexpected map level");
+    }
+    Leaflet.tileLayer(mapUrl, {
+      minZoom: 1,
+      maxZoom: 4,
+      center: [0, 0],
+      zoom: 1,
+    }).addTo(myMap);
+
+    var southWest = myMap.unproject([0, this.mapH], myMap.getMaxZoom()-1);
+    var northEast = myMap.unproject([this.mapW, 0], myMap.getMaxZoom()-1);
+    var bounds = new Leaflet.LatLngBounds(southWest, northEast);
+
+
+    Leaflet.imageOverlay(mapUrl, bounds).addTo(myMap);
+    myMap.setMaxBounds(bounds);
+
+    this.httpClient.post<dataItem>(this.apiURL+'histo/searchLast.php', {}/*JSON.stringify(credentials)*/).subscribe(
       data => {
         let markerGroup = Leaflet.featureGroup();
-    	  // let marker: any = Leaflet.marker([e.latitude, e.longitude], {icon:IconGreen}).bindPopup(customPopup,{closeButton:false})
-    	  for (let e of data.staff) {
-    		  var customPopup = "<strong>"+e.name+"</strong><br>"+e.locationX+" - "+e.locationY
-    		  let marker: any = Leaflet.marker([Number(e.locationX), Number(e.locationY)]/*{lat: e.latitude, lon: e.longitude}*/, {icon:IconGreen}).bindPopup(customPopup,{closeButton:false})
-    		  markerGroup.addLayer(marker);
-    	  }
-    	  this.map.addLayer(markerGroup);
-    		//console.log(data);
-    		//this.nav.setRoot('MenuPage');
+        // let marker: any = Leaflet.marker([e.latitude, e.longitude], {icon:IconGreen}).bindPopup(customPopup,{closeButton:false})
+        for (let e of data.staff) {if(Number(e.floor) != floor) continue;
+          var customPopup = "<strong>"+e.name+"</strong><br>"+e.locationX+" - "+e.locationY
+          let marker: any = Leaflet.marker([Number(e.locationX), Number(e.locationY)]/*{lat: e.latitude, lon: e.longitude}*/, {icon:(e.id==2)?this.IconRed:this.IconBlue}).bindPopup(customPopup,{closeButton:false})
+          markerGroup.addLayer(marker);
+        }
+        for (let e of data.matt) {if(Number(e.floor) != floor) continue;
+          var customPopup = "<strong>"+e.name+"</strong><br>"+e.locationX+" - "+e.locationY
+          let marker: any = Leaflet.marker([Number(e.locationX), Number(e.locationY)]/*{lat: e.latitude, lon: e.longitude}*/, {icon:this.IconGreen}).bindPopup(customPopup,{closeButton:false})
+          markerGroup.addLayer(marker);
+        }
+        myMap.addLayer(markerGroup);
+        //console.log(data);
+        //this.nav.setRoot('MenuPage');
       }, 
       error => { 
         console.log(error);
       }
     );
+
+    myMap.on('click', (e)=>{this.onMapClick(e)});
+
+  	// this.httpClient.post<dataItem>(this.apiURL+'histo/searchLast.php', {}/*JSON.stringify(credentials)*/).subscribe(
+   //    data => {
+   //      let markerGroup = Leaflet.featureGroup();
+   //  	  // let marker: any = Leaflet.marker([e.latitude, e.longitude], {icon:IconGreen}).bindPopup(customPopup,{closeButton:false})
+   //  	  for (let e of data.staff) {
+   //  		  var customPopup = "<strong>"+e.name+"</strong><br>"+e.locationX+" - "+e.locationY
+   //  		  let marker: any = Leaflet.marker([Number(e.locationX), Number(e.locationY)]/*{lat: e.latitude, lon: e.longitude}*/, {icon:this.IconGreen}).bindPopup(customPopup,{closeButton:false})
+   //  		  markerGroup.addLayer(marker);
+   //  	  }
+   //  	  this.map.addLayer(markerGroup);
+   //  		//console.log(data);
+   //  		//this.nav.setRoot('MenuPage');
+   //    }, 
+   //    error => { 
+   //      console.log(error);
+   //    }
+   //  );
 
     // var customPopup = "<strong>"+this.username+"</strong><br>"+10+" - "+10;
   	// let markerGroup = Leaflet.featureGroup();
@@ -111,13 +188,22 @@ export class MapPage {
   onLevelChanged(segmentButton){
     switch (segmentButton.value) {
       case "level1":
-        console.log("displayLevel1()");
+        console.log("showing level1");
+        document.getElementById("map1").style.display = "block";
+        document.getElementById("map2").style.display = "none";
+        document.getElementById("map3").style.display = "none";
         break;
       case "level2":
-        console.log("displayLevel2()");
+        console.log("showing level2");
+        document.getElementById("map1").style.display = "none";
+        document.getElementById("map2").style.display = "block";
+        document.getElementById("map3").style.display = "none";
         break;
       case "level3":
-        console.log("displayLevel3()");
+        console.log("showing level3");
+        document.getElementById("map1").style.display = "none";
+        document.getElementById("map2").style.display = "none";
+        document.getElementById("map3").style.display = "block";
         break;      
       default:
         throw new Error("Unexpected segment value");
