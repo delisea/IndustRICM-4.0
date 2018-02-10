26ea96bc6804c2b343a8e555bc0e853ac328c13d
Shloumpf <code.shloumpf@gmail.com>
2018-02-10 04:21:52 +0100

    End Alert + Notif

--- api/maintenance/alert.php
+++ api/maintenance/alert.php
@@ -7,33 +7,20 @@ include_once '../config/database.php';
 $database = new Database();
 $db = $database->getConnection();
 
-$query = 'SELECT id, locationX, locationY FROM Staff LEFT JOIN (SELECT `HistoS`.* FROM `HistoS`, (SELECT `idStaff`, MAX(`date`) AS MD FROM `HistoS` GROUP BY `idStaff`) AS GB WHERE `HistoS`.date=GB.MD AND `HistoS`.idStaff=GB.idStaff) AS FI ON Staff.id = FI.idStaff WHERE working = 1 AND idStaff IS NOT NULL';
+$postdata = $_POST['params'];
+$query = 'SELECT END.id FROM (SELECT ST.id, SQRT((ST.locationX-MA.locationX)*(ST.locationX-MA.locationX)+(ST.locationY-MA.locationY)*(ST.locationY-MA.locationY)) AS distance FROM (SELECT id, locationX, locationY FROM Staff LEFT JOIN (SELECT HistoS.idStaff, date, locationX, locationY FROM HistoS, (SELECT idStaff, MAX(date) AS MD FROM HistoS GROUP BY idStaff) AS GB WHERE HistoS.date=GB.MD AND HistoS.idStaff=GB.idStaff) AS FI ON Staff.id = FI.idStaff WHERE working = 1 AND idStaff IS NOT NULL) AS ST, (SELECT locationX, locationY FROM `HistoE`, (SELECT `idEquipment`, MAX(`date`) AS MD FROM `HistoE` WHERE idEquipment=:idEquipment GROUP BY `idEquipment`) as GB WHERE `HistoE`.date=GB.MD AND `HistoE`.idEquipment=GB.idEquipment) AS MA ORDER BY distance ASC LIMIT 1) AS END';
 $stmt = $db->prepare($query);
-$stmt->execute();
-$result = array();
-$result['staff'] = array();
-while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-	extract($row);
-	$staff_item = array(
-		'id' => $id,
-		'locationX' => $locationX,
-		'locationY' => $locationY
-	);
-	array_push($result['staff'], $staff_item);
-}
-$postdata = file_get_contents('php://input');
 $params = json_decode($postdata);
-$idEquipment = htmlspecialchars(strip_tags($params->id));
-$query = 'SELECT locationX, locationY FROM `HistoE`, (SELECT `idEquipment`, MAX(`date`) AS MD FROM `HistoE` WHERE idEquipment=:idEquipment GROUP BY `idEquipment`) as GB WHERE `HistoE`.date=GB.MD AND `HistoE`.idEquipment=GB.idEquipment';
-$stmt = $db->prepare($query);
-$stmt->bindParam(':idEquipment', $idEquipment);
+$temp = htmlspecialchars(strip_tags($params->id));
+$stmt->bindParam(':idEquipment', $temp);
 $stmt->execute();
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
 extract($row);
-$result['machine'] = array();
-$machine_item = array(
-	'locationX' => $locationX,
-	'locationY' => $locationY
+$staff_item = array(
+	'id' => intval($id)
 );
-array_push($result['machine'], $machine_item);
-echo json_encode($result);
+$query = 'INSERT INTO Maintenance SET idEquipment=:idEquipment, dateBreak=CURRENT_TIMESTAMP, idStaff=:idStaff, syn=0';
+$stmt = $db->prepare($query);
+$stmt->bindParam('idEquipment', $temp);
+$stmt->bindParam('idStaff', $staff_item['id']);
+$stmt->execute();
