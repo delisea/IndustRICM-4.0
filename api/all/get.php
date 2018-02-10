a53e77f8293dba17cff46969bae409664135868e
Unknown <lemaire.timothee@gmail.com>
2018-02-10 04:37:27 +0100

    all/get.php OK

--- api/all/get.php
+++ api/all/get.php
@@ -4,6 +4,13 @@ header("Content-Type: application/json; charset=UTF-8");
 
 include_once '../config/database.php';
 
+ob_start();
 include_once '../equipment/get.php';
+ob_start();
 include_once '../staff/get.php';
+$staff = ob_get_clean();
+$equipment = ob_get_clean();
+
+$all=json_encode(array_merge_recursive(json_decode($equipment, true),json_decode($staff, true)));
+echo $all;
 ?>
